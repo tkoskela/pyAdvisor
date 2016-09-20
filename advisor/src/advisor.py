7ad9c7dfaa17ff395e7f366997d69f3a454d7054
mathieu_lobet <mathieu.lobet@gmail.com>
2016-09-20 13:40:44 -0500

    1) Creation of the functions to compute and manage the structure loops_with_data (list of dictionaries) containing loops with data from loops

--- advisor/src/advisor.py
+++ advisor/src/advisor.py
@@ -22,7 +22,7 @@ class loop():
         """
         Returns True if both AI and GFLOPS strings have length greater than 0
         """
-        if len(self.ai) > 0 and len(self.gflops) > 0:
+        if len(self.ai) > 0 and (self.ai[0:1] != '<') and len(self.gflops) > 0:
             return True
         else:
             return False
@@ -86,6 +86,197 @@ class advisor_results():
                 self.data[self.keys[j]].append(val)
                 
         self.labels = self.data['Function Call Sites and Loops']
+
+    def parse_functioncallsitesandloops(self,loop_with_data):
+        """
+        This method parses the child property, the subroutine, the file and the line number 
+        for each loop with data from the functioncallsitesandloops parameter.
+
+        Inputs:
+        -------
+        loop_with_data: list - list of loops with data, loops are dictionnaries
+        -------        
+        """
+
+        # functioncallsitesandloops
+        fcsal = loop_with_data['fcsal']
+        child = False
+        subroutine = ''
+        file = ''
+        line = 0
+
+        # Get the child property
+        if fcsal[1:6] == 'child':
+            child = True
+            fcsal = fcsal[16:-1]
+        else:
+            fcsal = fcsal[8:-1]
+
+        # Get the subroutine that contains the loop
+        fcsal = fcsal.split(' at ')
+        subroutine = fcsal[0]
+
+        # Get the file
+        fcsal = fcsal[1].split(':')
+        file = fcsal[0]
+
+        # get the line number
+        line = int(fcsal[1])
+
+        loop_with_data['child'] = child
+        loop_with_data['subroutine'] = subroutine
+        loop_with_data['file'] = file
+        loop_with_data['line'] = line
+
+    def compute_loops_with_data(self):
+        """
+        This method computes the list of loops with data. Each loop is represented by a dictionnary.
+        """
+
+        # Creation of the list of loop with data
+        self.loops_with_data = []
+        k = 0
+        for loop in self.loops:
+            if loop.has_data():
+                temp = {}
+                temp['id'] = k
+                temp['ai'] = float(loop.ai)
+                temp['gflops']=(float(loop.gflops))
+                temp['time']=(float(loop.selftime[:-1]))
+                if 'Vectorized' in loop.type:
+                    temp['gain'] = float(loop.gainestimate[:-1])
+                    temp['type'] = 'Vectorized'
+                else:
+                    temp['gain'] = 0.
+                    temp['type'] = 'Scalar'
+                temp['fcsal'] = loop.functioncallsitesandloops
+                self.parse_functioncallsitesandloops(temp)
+                self.loops_with_data.append(temp)
+                k += 1
+
+            elif loop.child_has_data():
+                for child in loop.children:
+                    if child.has_data():
+                        temp = {}
+                        temp['id'] = k
+                        temp['ai'] = float(child.ai)
+                        temp['gflops']=(float(child.gflops))
+                        temp['time']=(float(child.selftime[:-1]))
+                        if 'Vectorized' in child.type:
+                            temp['gain'] = float(loop.gainestimate[:-1])
+                            temp['type'] = 'Vectorized'
+                        else:
+                            temp['gain'] = 0.
+                            temp['type'] = 'Scalar'
+                        temp['fcsal'] = child.functioncallsitesandloops
+                        self.parse_functioncallsitesandloops(temp)
+                        self.loops_with_data.append(temp)
+                        k += 1
+
+    def remove_in_loops_with_data(self,file=None):
+        """
+        Remove loops from the list of loops with data according to the provided filter arguments.
+
+        Inputs:
+        -------
+        file : string - fine name
+
+        """
+        n = 0
+        for i,loop in enumerate(self.loops_with_data):
+            if loop['file'] in file:
+                self.loops_with_data.pop(i)   
+                n+=1
+        print(' In remove_in_loops_with_data: {0} loops have been deleted'.format(n)) 
+
+    def sort_loops_with_data(self,key='file'):
+        """
+        Sort the list of loops according to the specified key.
+
+        Inputs:
+        -------
+        key : string - key ot be used for sorting
+
+        """
+        self.loops_with_data = sorted(self.loops_with_data, key=lambda k: k[key])
+
+    def get_properties_from_loops_with_data(self,file=None,line=None):
+        """
+        Convert the list of loops into several lists for each property (in a sens, go from AoS to SoA).
+        Filter options can be specified in input.
+
+        Inputs:
+        -------
+        file : string - file name filter
+        line : integer - line number filter
+
+        Output:
+        -------
+        ai         : list - list of arithmetic intensities
+        gflops     : list - list of gflops
+        subroutine : list - list of subroutines
+        """
+
+        # Creation of the lists
+        lid = list()
+        ai = list()
+        gflops = list()
+        subroutines = list()
+        times = list()
+
+        # Fill the lists
+        for loop in self.loops_with_data:
+            if ((file == None)or(loop['file'] in file)):
+                if ((line == None)or(loop['line']in line)):
+                    lid.append(loop['id'])
+                    ai.append(loop['ai'])
+                    gflops.append(loop['gflops'])
+                    times.append(loop['time'])
+                    subroutines.append(loop['subroutine'])
+        return lid,ai,gflops,times,subroutines
+
+    def print(self):
+        """
+        This method prints in the terminal the properties of the loops with data.
+        """
+        # Number of loops with data
+        nloops = len(self.loops_with_data)
+
+        print(' \n')
+        print(' List of loops with data:')
+        print(' Number of loops: {0:2} \n'.format(nloops))
+        print(' {0:2}: {1:20} {2:45} {3:5} {4:10} {5:10} {6:10} {7:10} {8:10}'.format('id','file','subroutine','line','Flop/byte','Gflop/s','Time (s)','gain','type'))
+        print(' -------------------------------------------------------------------------------------------------------------------------')
+
+        for i in range(nloops):
+            loop = self.loops_with_data[i]
+            print(' {0:2d}: {1:20} {2:45} {3:5} {4:10} {5:10} {6:10} {7:10} {8:10}'.format(loop['id'],loop['file'][0:20],loop['subroutine'][0:45],loop['line'],loop['ai'],loop['gflops'],loop['time'],loop['gain'],loop['type']))
+
+
+    def roofline(self,fig,ax,x,dram_bandwidth=None,mcdram_bandwidth=None,scalar_gflops=None,dp_vect_gflops=None):
+        """
+        Plot the rooflines according to the given numbers
+
+        Input
+        ------
+        fig              : figure object
+        ax               : figure axis
+        x                : array of floats - array of abscissa (arithmetic intensities in flops/byte)
+        dram_bandwidth   : float - dram bandwidth in gbytes/s
+        mcdram_bandwidth : float - mcdram bandwidth in gbytes/s
+        ------
+        """
+        # Dram roofline
+        xlim = np.array(ax.get_xlim())
+        ylim = np.array(ax.get_ylim())
+
+        if dram_bandwidth != None:
+            if scalar_gflops != None:
+                y = np.minimum(np.ones(len(x)) * scalar_gflops , x * dram_bandwidth)
+                ax.plot(x,y,color='k',ls='-',lw='2')
+            if dp_vect_gflops != None:
+                y = np.minimum(np.ones(len(x)) * dp_vect_gflops , x * dram_bandwidth)
+                ax.plot(x,y,color='k',ls='-',lw='2')
 
     def plot(self,fignum=1,markersize=200,mrk='o',newfig=True,label=None,tooltips=True):
         """
